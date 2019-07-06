68e46b9773e860cf634e17359ca42bb554aee601
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-07-06 13:09:01 +0200

    Improve test with a fixture

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
 
-../wasmer/target/release/wasmer run xz.wasm
+# ../wasmer/target/release/wasmer run lzmadec.wasm
+# ../wasmer/target/release/wasmer run lzmainfo.wasm
+# ../wasmer/target/release/wasmer run -- xz.wasm -cd fixture.xz
+../wasmer/target/release/wasmer run -- xzdec.wasm -c fixture.xz
